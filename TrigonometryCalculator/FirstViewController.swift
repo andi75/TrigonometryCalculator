19ba136ee44b05cf0d423e2b860f1a511cd07197
andi75 <umbach@gmail.com>
2015-07-27 18:39:35 +0200

    - created SSS mode (a, b, c given) and SWS mode (a, b, gamma given)

--- TrigonometryCalculator/FirstViewController.swift
+++ TrigonometryCalculator/FirstViewController.swift
@@ -9,10 +9,50 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    @IBOutlet weak var aValue: UITextField!
+    @IBOutlet weak var bValue: UITextField!
+    @IBOutlet weak var cValue: UITextField!
 
+    @IBOutlet weak var alpha: UILabel!
+    @IBOutlet weak var beta: UILabel!
+    @IBOutlet weak var gamma: UILabel!
+    
+    func setDefaultValues()
+    {
+        aValue.text = "3.0"
+        bValue.text = "4.0"
+        cValue.text = "5.0"
+    }
+    
+    func updateAlphaBetaGamma()
+    {
+        let a = NSString(string: aValue.text).doubleValue
+        let b = NSString(string: bValue.text).doubleValue
+        let c = NSString(string: cValue.text).doubleValue
+        
+        if((a <= 0.0) || (b <= 0.0) || (c <= 0.0) || (a + b < c) || (a + c < b) || (b + c < a))
+        {
+            alpha.text = "undefined"
+            beta.text = "undefined"
+            gamma.text = "undefined"
+        }
+        else
+        {
+            alpha.text = "\(180.0 / M_PI * acos( ( b*b + c*c - a*a ) / ( 2 * b * c ) ))"
+            beta.text = "\(180.0 / M_PI * acos( ( a*a + c*c - b*b ) / ( 2 * a * c ) ))"
+            gamma.text = "\(180.0 / M_PI * acos( ( a*a + b*b - c*c ) / ( 2 * a * b ) ))"
+        }
+    }
+    
+    @IBAction func editingChanged() {
+        updateAlphaBetaGamma()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setDefaultValues()
+        updateAlphaBetaGamma()
     }
 
     override func didReceiveMemoryWarning() {
--- TrigonometryCalculator/SecondViewController.swift
+++ TrigonometryCalculator/SecondViewController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var aValue: UITextField!
+    @IBOutlet weak var bValue: UITextField!
+    @IBOutlet weak var gammaValue: UITextField!
+    @IBOutlet weak var alpha: UILabel!
+    @IBOutlet weak var beta: UILabel!
+    @IBOutlet weak var cLabel: UILabel!
+    
+    func setDefaultValues()
+    {
+        aValue.text = "3.0"
+        bValue.text = "4.0"
+        gammaValue.text = "90.0"
+    }
+    
+    func updateAlphaBetaGamma()
+    {
+        let a = NSString(string: aValue.text).doubleValue
+        let b = NSString(string: bValue.text).doubleValue
+        let gamma = NSString(string: gammaValue.text).doubleValue
+        
+        if((a <= 0.0) || (b <= 0.0) || (gamma < 0.0) || (gamma > 180.0))
+        {
+            alpha.text = "undefined"
+            beta.text = "undefined"
+            cLabel.text = "undefined"
+        }
+        else
+        {
+            let cSquared = a * a + b * b - 2 * a * b * cos(gamma / 180.0 * M_PI)
+            let c = sqrt( cSquared )
+            alpha.text = "\(180.0 / M_PI * acos( ( b*b + c*c - a*a ) / ( 2 * b * c ) ))"
+            beta.text = "\(180.0 / M_PI * acos( ( a*a + c*c - b*b ) / ( 2 * a * c ) ))"
+            cLabel.text = "\(c)"
+        }
+    }
+    
+    
+    @IBAction func editingChanged() {
+        updateAlphaBetaGamma()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setDefaultValues()
+        updateAlphaBetaGamma()
     }
 
     override func didReceiveMemoryWarning() {
